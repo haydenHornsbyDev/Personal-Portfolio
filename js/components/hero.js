40d2f91ca71642a23b205273ca61ad0c360dca44
Hayden <haydenhornsby@hotmail.com>
2022-08-11 14:13:54 +0100

    added animation to hero element

--- js/components/hero.js
+++ js/components/hero.js
@@ -1,3 +1,16 @@
 (function() {
-    // misc js here
+    const heroes = document.querySelectorAll('[data-hero]');
+    const heroDividers = document.querySelectorAll('[data-hero-divider]');
+    const heroTexts = document.querySelectorAll('[data-hero-text]');
+
+    function loadHero() {
+        console.log('ss');
+        heroDividers.forEach(divider => divider.classList.add('is-active'));
+        heroTexts.forEach(text => text.classList.add('is-active'));
+    };
+    
+    window.addEventListener('DOMContentLoaded', (event) => {
+        console.log('DOM loaded');
+        setTimeout(loadHero, 1000);
+    });
 })();
--- js/script.js
+++
@@ -1,3 +0,0 @@
-(function() {
-    // misc js here
-})();
