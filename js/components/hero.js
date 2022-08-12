6d0965e0cdf1e5121eeb1aa60113e3b6271a194d
Hayden <haydenhornsby@hotmail.com>
2022-08-12 12:45:29 +0100

    reformatted to all be one page and added html for contact section

--- js/components/hero.js
+++ js/components/hero.js
@@ -2,11 +2,13 @@
     const heroes = document.querySelectorAll('[data-hero]');
     const heroDividers = document.querySelectorAll('[data-hero-divider]');
     const heroTexts = document.querySelectorAll('[data-hero-text]');
+    const heroArrows = document.querySelectorAll('[data-hero-arrow]');
 
     function loadHero() {
         console.log('ss');
         heroDividers.forEach(divider => divider.classList.add('is-active'));
         heroTexts.forEach(text => text.classList.add('is-active'));
+        heroArrows.forEach(arrow => arrow.classList.add('is-active'));
     };
     
     window.addEventListener('DOMContentLoaded', (event) => {
